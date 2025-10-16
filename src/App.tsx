d268120003b0c4eb97140e1dc15ab857998466b2
prajwalsiwa <prajwal.siwa1@gmail.com>
2025-10-16 09:02:13 +0545

    feat: setup tailwind css for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <>hello</>;
+  return (
+    <>
+      <div className="text-red-500">tailwind color check</div>
+    </>
+  );
 }
 
 export default App;
