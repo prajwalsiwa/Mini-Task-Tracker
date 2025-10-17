0a93c6962707c75c98f0bfa1e815b28dbfeb7527
prajwalsiwa <prajwal.siwa1@gmail.com>
2025-10-17 21:55:00 +0545

    feat: fetch get task mock api

--- src/pages/TaskTracker.tsx
+++ src/pages/TaskTracker.tsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
+import TaskList from "../components/TaskList";
+import type { Tasks } from "../types/types";
+import { getTasks } from "../api/TaskApi";
 
 function TaskTracker() {
+  const [tasks, setTasks] = useState<Tasks[] | []>([]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const tasks = await getTasks();
+      setTasks(tasks as Tasks[]);
+    };
+
+    fetchTasks();
+  }, []);
+
+  console.log(tasks, "tasks");
+
   return (
     <div className=" text-slate-800 dark:text-slate-200 w-full transition-colors duration-300">
       <div className="container mx-auto max-w-4xl p-4 md:p-8">
         <Header onAddTask={() => {}} />
+        <div className=" bg-white dark:bg-slate-800 rounded-xl shadow-lg overflow-hidden">
+          <TaskList tasks={tasks} />
+        </div>
       </div>
     </div>
   );
